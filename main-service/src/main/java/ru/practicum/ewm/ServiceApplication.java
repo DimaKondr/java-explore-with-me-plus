87510d0008ca7aaab4f14ee4f0f19e655803425e
Dima Kondratyev <timon.4eb@yandex.ru>
2026-03-18 21:05:40 +0300

    Создан каркас приложения. В сервис статистики добавлены DTO.

---
+++ main-service/src/main/java/ru/practicum/ewm/ServiceApplication.java
@@ -0,0 +1,25 @@
+package ru.practicum.ewm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class ServiceApplication {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(ServiceApplication.class, args);
+
+        //StatClient statClient = context.getBean(StatClient.class);
+        //statClient.hit(new HitDto(null, "ewm-main-service", "event/1", "192.163.0.1", "2022-09-06 11:00:23"));
+        //StatResponseDto stat = statClient.getStat(new StatRequestParamDto(
+        // "2022-01-01 00:00:00",
+        // "2023-12-31 23:59:59",
+        // List.of("event/1"),
+        // false
+        // ));
+        //System.out.println(stat);
+
+    }
+
+}
---
+++ stat/stat-dto/src/main/java/ru/practicum/ewm/HitDto.java
@@ -0,0 +1,16 @@
+package ru.practicum.ewm;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class HitDto {
+    private Long id;
+    private String app;
+    private String uri;
+    private String ip;
+    private String timeStamp;
+}
