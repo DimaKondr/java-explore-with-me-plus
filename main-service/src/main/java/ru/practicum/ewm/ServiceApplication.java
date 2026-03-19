07f0dbc06caffcbb72c5dbc8eca935f75941b464
Ilux-i <iluxeo6@gmail.com>
2026-03-19 17:59:45 +0300

    feature
    Подредачил помники и докер файл, чтоб не подали сервисы при запуске
    Реализовал клиент статистики
    Добавил кконфигурацию для создания бина в основном сервисе

--- main-service/src/main/java/ru/practicum/ewm/ServiceApplication.java
+++ main-service/src/main/java/ru/practicum/ewm/ServiceApplication.java
@@ -10,15 +10,15 @@ public class ServiceApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(ServiceApplication.class, args);
 
-        //StatClient statClient = context.getBean(StatClient.class);
-        //statClient.hit(new HitDto(null, "ewm-main-service", "event/1", "192.163.0.1", "2022-09-06 11:00:23"));
-        //StatResponseDto stat = statClient.getStat(new StatRequestParamDto(
-        // "2022-01-01 00:00:00",
-        // "2023-12-31 23:59:59",
-        // List.of("event/1"),
-        // false
-        // ));
-        //System.out.println(stat);
+//        StatClient statClient = context.getBean(StatClient.class);
+//        statClient.postHit(new HitDto(null, "ewm-main-service", "event/1", "192.163.0.1", "2022-09-06 11:00:23"));
+//        StatResponseDto stat = statClient.getStats(new StatRequestParamDto(
+//         "2022-01-01 00:00:00",
+//         "2023-12-31 23:59:59",
+//         List.of("event/1"),
+//         false
+//         ));
+//        System.out.println(stat);
 
     }
 
