10891319e7dc0b9aac3642453de3430e070fae83
BelovEvgenii <Zhekab777@yandex.ru>
2026-03-19 17:09:03 +0300

    fix(Dto): добавить в pom зависимость lombok style: chekstyle

--- stat/stat-service/src/main/java/ru/practicum/ewm/error/ErrorHandler.java
+++ stat/stat-service/src/main/java/ru/practicum/ewm/error/ErrorHandler.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.Map;
 
--- stat/stat-service/src/main/java/ru/practicum/ewm/repository/HitRepository.java
+++ stat/stat-service/src/main/java/ru/practicum/ewm/repository/HitRepository.java
@@ -4,8 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import ru.practicum.ewm.model.Hit;
 import ru.practicum.ewm.StatResponseDto;
+import ru.practicum.ewm.model.Hit;
 
 import java.time.LocalDateTime;
 import java.util.List;
