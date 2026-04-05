be330c98f95c8b903b23641d12a47ac6e9f03e48
Dima Kondratyev <timon.4eb@yandex.ru>
2026-04-05 17:32:17 +0300

    В сервисе статистики в методе сохранения записи сделано мгновенной сохранение записи в БД.

--- stat/stat-service/src/main/java/ru/practicum/ewm/service/StatServiceImpl.java
+++ stat/stat-service/src/main/java/ru/practicum/ewm/service/StatServiceImpl.java
@@ -28,7 +28,7 @@ public class StatServiceImpl implements StatService {
     public HitDto createHit(HitDto hitDto) {
         log.info("Сохранение информации о запросе: {}", hitDto);
         Hit hit = hitMapper.toEntity(hitDto);
-        Hit savedHit = hitRepository.save(hit);
+        Hit savedHit = hitRepository.saveAndFlush(hit);
         return hitMapper.toDto(savedHit);
     }
 
